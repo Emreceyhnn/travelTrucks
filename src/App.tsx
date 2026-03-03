ed8d39b4f45bbef20ba806f4dfa2801076e6d33c
Emre Ceyhan <jemreceyhnn@gmail.com>
2026-03-03 20:45:15 +0300

    t

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 import { store } from './redux/store';
 import Header from './components/Header/Header';
-import HomePage from './pages/HomePage/HomePage';
-import CatalogPage from './pages/CatalogPage/CatalogPage';
-import CamperDetailPage from './pages/CamperDetailPage/CamperDetailPage';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage'));
+const CamperDetailPage = lazy(() => import('./pages/CamperDetailPage/CamperDetailPage'));
 
 const App: React.FC = () => {
   return (
@@ -13,11 +15,13 @@ const App: React.FC = () => {
       <BrowserRouter>
         <Header />
         <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/catalog" element={<CatalogPage />} />
-            <Route path="/catalog/:id" element={<CamperDetailPage />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/catalog" element={<CatalogPage />} />
+              <Route path="/catalog/:id" element={<CamperDetailPage />} />
+            </Routes>
+          </Suspense>
         </main>
         <Toaster position="top-right" />
       </BrowserRouter>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-mui': ['@mui/material', '@mui/icons-material', '@emotion/react', '@emotion/styled'],
+          'vendor-redux': ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  },
 })
